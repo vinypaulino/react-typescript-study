1167521d6f543399a1318fa50cb2d8d7579bd7d9
Vini Paulino <vinydos3@gmail.com>
2023-03-20 21:24:28 -0300

    refactor: adds css module to avoid overlap

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,14 +1,18 @@
 import React from "react";
-import "./style.scss";
+import style from "./List.module.scss";
 
 function List() {
-  const tasks = [{ tarefa: 'React', tempo: '02:00:00' }, { tarefa: 'Javascript', tempo: '01:00:00' }, { tarefa: 'Software Architecture', tempo: '02:00:00' }]
+  const tasks = [
+          { tarefa: 'React', tempo: '02:00:00' }, 
+          { tarefa: 'Javascript', tempo: '01:00:00' }, 
+          { tarefa: 'Software Architecture', tempo: '02:00:00' }]
+  
   return (
-    <aside className="tasksList">
+    <aside className={style.tasksList}>
       <h2> Estudos do Dia </h2>
       <ul>
         {tasks.map((item, index) => (
-          <li key={index} className="item "> 
+            <li key={index} className={style.item}> 
             <h3>{item.tarefa}</h3>
             <span>{item.tempo}</span>
           </li>
