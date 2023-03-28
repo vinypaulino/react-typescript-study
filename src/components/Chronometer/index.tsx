8882b6fe899460ff6323f6b89c7028e3948ea495
vinypaulino <vinydos3@gmail.com>
2023-03-27 21:59:41 -0300

    feat: creates chronometer component

---
+++ src/components/Chronometer/index.tsx
@@ -0,0 +1,20 @@
+import Button from "../Button";
+import style from "./Chronometer.module.scss";
+import Clock from "./Clock";
+
+export function Chronometer() {
+    return (
+        <>
+            <div className={style.chronometer}>
+                <p className={style.title}> Escolha um card e inicie o cronômetro </p>
+
+                <div className={style.clockwrapper}>
+                    <Clock/>
+                </div>
+                <Button>
+                    Começar!
+                </Button>
+            </div>
+        </>
+    )
+}
