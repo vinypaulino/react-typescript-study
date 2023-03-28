5e6c4ffb6cef5599ea79063a91b8f8d2b2896899
vinypaulino <vinydos3@gmail.com>
2023-03-27 22:00:05 -0300

    feat: adds chronometer on app

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Chronometer } from '../components/Chronometer';
 import Form from '../components/Form';
 import List from '../components/List'
 import style from './App.module.scss';
@@ -9,6 +10,7 @@ function App() {
     <div className={style.AppStyle}>
             <Form /> 
             <List />
+            <Chronometer />
     </div>
   );
 }
