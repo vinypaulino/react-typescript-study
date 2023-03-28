5f371d8a373451865d5967ffa5f0602576e011ed
vinypaulino <vinydos3@gmail.com>
2023-03-28 19:20:56 -0300

    feat: adds hook to maneger state

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 import style from "./List.module.scss";
 
 function List() {
-  const tasks = [
+  const [tasks, setTasks] = useState([
           { task: 'React', time: '02:00:00' }, 
           { task: 'Javascript', time: '01:00:00' }, 
-          { task: 'Software Architecture', time: '02:00:00' }]  
+          { task: 'Software Architecture', time: '02:00:00' }])  
   
   return (
     <aside className={style.tasksList}>
-      <h2> Estudos do Dia </h2>
+      <h2 onClick={() => {
+        setTasks([...tasks, {task: "Estudar estado", time: "05:00:00"}])
+      }}> Estudos do Dia </h2>
       <ul>
         {tasks.map((item, index) => (
            <Item key={index} {...item}/>
